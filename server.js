635000d0ae6878c98125009e99ea76fdfc2e17e6
joelc95 <joelconalty@gmail.com>
2022-11-15 20:14:58 +0000

    added spotify log in via redirect

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import fetch from "node-fetch";
 import express from "express";
 
-const spotify_client_id = "";
-const spotify_client_secret = "";
+const spotify_client_id = "b32001087f804ee7a69d045e5a1ca07c";
+const spotify_client_secret = "1c13e2cfa9df4030a0e4670257bb115a";
 const spotify_redirect_uri = "http://localhost:3000/callback";
 
 const app = express();
@@ -17,19 +17,20 @@ app.get("/", function (request, response) {
 });
 
 app.get("/authorize", (req, res) => {
-  console.log("authorize");
+  // http://api.spotify.com/endpoint?key=val&key=val...
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: "",
+    redirect_uri: spotify_redirect_uri
+  })
+
+  res.redirect('https://accounts.spotify.com/authorize?' + auth_query_parameters.toString());
 });
 
-app.get("/callback", async (req, res) => {
-  console.log("callback");
-});
-
-app.get("/logout", function (request, response) {
-  response.redirect("/");
-});
-
-app.get("/dashboard", async function (req, res) {
-  console.log("callback");
+app.get("/callback", (req, res) => {
+  const code = req.query.code;
+  console.log(code);
 });
 
 let listener = app.listen(3000, function () {
